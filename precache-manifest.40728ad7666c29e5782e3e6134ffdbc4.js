4ed003f4827dcfae665fafe1d230311eb71728ea
Jesse Zarat <jczarat@gmail.com>
2020-10-16 14:38:07 -0600

    Updates

---
+++ precache-manifest.40728ad7666c29e5782e3e6134ffdbc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034f65c43ec20a8548fb026378e7a036",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "e95ab3039cc20c4f9832",
+    "url": "/project-4-assessment/static/css/main.2ef096e1.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95ab3039cc20c4f9832",
+    "url": "/project-4-assessment/static/js/main.47ac8312.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
